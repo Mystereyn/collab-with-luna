d4a97219090ef4cc004315b39f61695629b68782
Mystereyn <teukureynaldi106@gmail.com>
2023-06-01 15:12:15 +0700

    note : the result still bug and node showed the result is an object, but i'm not creating any object

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-
-
 const readline = require('readline'); //console.info(readline);
 // let input = readline.getPrompt();
-let input = readline.createInterface(process.stdin, process.stdout);
 
-function getUserWant(){
-    "use strict";
 
-    input = readline.question("Type any todo today : ");
-    return `so, you are doing an activities : ${input};`;
+let streamInput = readline.createInterface({
+    input : process.stdin, 
+    output : process.stdout
+});
+
+function readInput(){
+    // "use strict";
+
+    streamInput.question("Type any todo today : ")
+    console.log("Your todo is : " +  streamInput);
 
 }
 
-const print = getUserWant();
-console.log(print);
+const print = readInput();
+console.log(print)
